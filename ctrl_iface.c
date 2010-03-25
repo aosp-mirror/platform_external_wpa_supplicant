97168b54aa9d793599ec95515296e9149bd920a0
Dmitry Shmidt <dimitrysh@google.com>
2010-03-25 14:22:48 -0700

    Fix status from ap_scan command
    Change-Id: Ib7bf2e9ef6f602ea0f95314cb2db7b3d42276c31
    Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>

--- ctrl_iface.c
+++ ctrl_iface.c
@@ -1134,7 +1134,7 @@ static int wpa_supplicant_ctrl_iface_ap_scan(
 		return -1;
 #ifdef ANDROID
 	if ((ap_scan == 2) && (wpa_s->wpa_state != WPA_COMPLETED)) {
-		return -1;
+		return 0;
 	}
 #endif
 	wpa_s->conf->ap_scan = ap_scan;
